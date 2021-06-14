1a84009fefdc0ceb6fdb8af6608ec8753ee16ee2
GoldenGladier <omar.fi.wwr@gmail.com>
2021-06-14 14:46:28 -0500

    Omar mejoro los ejercicios tipo 1

--- src/components/level1.js
+++ src/components/level1.js
@@ -1,12 +1,14 @@
 import React from "react";
 import './css/level.css';
 // IMPORTANDO COMPONENTES
-import TableBox from './tableBox';
-import ResultBox from './resultsBox';
+import { Link } from "react-router-dom";
+import TableBox1 from './tableBox';
+import TableBox2 from './tableBox2';
+import ResultBox1 from './resultsBox';
+import ResultBox2 from './resultsBox2';
 import swal from 'sweetalert';
 // PARA TRAER LOS DATOS Y PROCESARLOS
 import axios from "axios";
-// import debounce from 'lodash.debounce';
 
 class Level extends React.Component {
     state = {
@@ -83,6 +85,7 @@ class Level extends React.Component {
             if(isNaN(this.state.sizew1)){
                 var numero = input2 + this.state.sizew1;
                 this.setState({square3:numero});
+                console.log("Incognita");
             }
             else{
                 var n1 = parseInt(input2);
@@ -120,10 +123,31 @@ class Level extends React.Component {
                 console.log("Revisando: Mal");
             }
         }
+        else{
+            swal("¡Faltan datos!", "Debes llenar ambos campos para completar los dos binomios.", "info");
+        }
     }
 
     render() {
-
+        let tablero = '';
+        // ========= DIBUJANDO LA CAJA/TABLERO ========= 
+        if(parseInt(this.props.tipo) == 1) {   
+            tablero = (
+            <TableBox1 width={width} height={height} sizeh1={sizeh1} sizeh2={sizeh2_answer} 
+                equation1={sizew1} equation2={sizew2_answer} square1={this.state.square1} square2={square2}
+                square3={this.state.square3} square4={square4} binom1={this.state.binom1}
+                handleInput1Change={this.handleInput1Change} handleInput2Change={this.handleInput2Change}
+            ></TableBox1>  
+            );
+        }
+        else{
+            <TableBox2 width={width} height={height} sizeh1={sizeh1} sizeh2={sizeh2_answer} 
+                equation1={sizew1} equation2={sizew2_answer} square1={this.state.square1} square2={square2}
+                square3={this.state.square3} square4={square4} binom1={this.state.binom1}
+                handleInput1Change={this.handleInput1Change} handleInput2Change={this.handleInput2Change}
+            ></TableBox2>  
+        }
+        
         const { 
             nombre, tipo, width, height, sizeh1, sizeh2_answer, sizew1, sizew2_answer, 
             square1, square2, square3, square4, expresion, respuesta
@@ -136,24 +160,20 @@ class Level extends React.Component {
                         <span class="navbar-toggler-icon"></span>
                         </button>
                         <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-                        <a class="navbar-brand" href="#">Level 1</a>
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                            <a class="nav-link" href="#">Challenge 1 of 6</a>
-                            </li>
-                            <li class="nav-item">
-                            <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">
-                            Score: 
-                                <i class="bi bi-star-fill"></i> <i class="bi bi-star"></i> <i class="bi bi-star"></i> <i class="bi bi-star"></i>  
-                            </a>
-                            </li>
-                        </ul>
-                        <button class="btn btn-outline-light" type="submit">Start Over</button>      
+                            <h1 class="navbar-brand">{nombre}</h1>
+                            {/* <Link to="/Algebra_model_game/" class="btn btn-outline-light">Volver al inicio</Link>                             */}
+                            {/* <form class="d-flex">
+                                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                                <button class="btn btn-outline-success" type="submit">Search</button>
+                            </form> */}
                         </div>
+                        <span class="navbar-text">
+                            <Link to="/Algebra_model_game/" class="btn btn-outline-light">Volver al inicio</Link>                            
+                        </span>                        
                     </div>
                 </nav>
                 <h1>Busca el producto parcial.</h1>
-                <h2>{nombre}</h2>
+                {/* <h2>{nombre}</h2> */}
                 {/* <h2>Width: {width}, Height: {height}</h2>                 */}
                 {/* <div className="content center-all"> */}
 
