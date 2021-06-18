44e455f84671b8da65bdd5bfe8a82702c6aa87be
GoldenGladier <omar.fi.wwr@gmail.com>
2021-06-17 19:10:28 -0500

    Corregi la redereccion de borrar pregunta

--- src/components/pregunta.js
+++ src/components/pregunta.js
@@ -1,12 +1,24 @@
 import React from "react";
 // import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+var Navigation = require('react-router').Navigation;
 import axios from "axios";
 import swal from "sweetalert";
+import { Redirect } from 'react-router';
+import {browserHistory} from "react-router";
+import {history} from "react-router";
+import ReactDOM from 'react-dom'
+import { withRouter } from 'react-router-dom';
 
-const Pregunta = ({ id, nombre }) => {
-
-    const handleClickEliminar = (event) => {
+class Pregunta extends React.Component{
+    constructor(props) {
+        super(props)
+        this.handleClickEliminar = this.handleClickEliminar.bind(this)
+        this.state = {
+            url: ''                   
+        }        
+    }
+    handleClickEliminar = (event) => {
         //Eliminar
 
         swal({title: "Eliminar", 
@@ -15,9 +27,9 @@ const Pregunta = ({ id, nombre }) => {
         buttons: ["No","Confirmar"]
         }).then(respuesta => { // Se ejecuta al darle a confirmar
             if(respuesta){
-                console.info(id + " llamo a la función eliminar.")
+                console.info(this.props.id + " llamo a la función eliminar.")
                 // Se envia el delete al backend
-                axios.get(`/Algebra_model_game/deleteQuestion?idList=${id}`).then(response => {
+                axios.get(`/Algebra_model_game/deleteQuestion?idList=${this.props.id}`).then(response => {
                     console.info(response.data);
                     swal({ text: "La pregunta se ha eliminado exitosamente", // Notificacion
                     icon:"success",
@@ -26,38 +38,42 @@ const Pregunta = ({ id, nombre }) => {
                 }).catch(error => {
                     console.info(error);
                 }).finally(() => { // Recarga el crud
-                    window.location.href = "/Algebra_model_game/home";
-                });
+                    this.setState({url: "/Algebra_model_game/home"});
+            });
 
                 
             }else{
-                window.location.href = "/Algebra_model_game/home"; // Recara el crud
+                this.setState({url: "/Algebra_model_game/home"}); // Recara el crud
             }
         })
     }
 
-
+    render(){
+    if(this.state.url != ''){
+            return (<Redirect to={this.state.url} />);
+    }        
     return (
         <tr>
-            <td>{nombre}</td>
+            <td>{this.props.nombre}</td>
             <td className="AlignCenter">
-                <Link to={`/Algebra_model_game/view?id=${id}`} className="btn btn-success M-6 CustomLink">
+                <Link to={`/Algebra_model_game/view?id=${this.props.id}`} className="btn btn-success M-6 CustomLink">
                     Ver ejercicio
                 </Link>
 
-                <Link to={`/Algebra_model_game/update?id=${id}`} className="btn btn-warning M-6 CustomLink" >
+                <Link to={`/Algebra_model_game/update?id=${this.props.id}`} className="btn btn-warning M-6 CustomLink" >
                     Modificar ejercicio
                 </Link>
                 
-                <Link onClick={handleClickEliminar} className="btn btn-danger M-6 CustomLink">
+                <Link onClick={this.handleClickEliminar} className="btn btn-danger M-6 CustomLink">
                     Eliminar ejercicio
                 </Link>       
 
-                <Link to={`/Algebra_model_game/level?id=${id}`} className="btn btn-info M-6 CustomLink">
+                <Link to={`/Algebra_model_game/level?id=${this.props.id}`} className="btn btn-info M-6 CustomLink">
                     Probar ejercicio
                 </Link>                         
             </td>
         </tr>
-    )
+        );
+    }
 }
 export default Pregunta;
